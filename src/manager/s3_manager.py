60aad100a8590de22f19d2f1b0258f649f3f2bae
lukasdrothler <lukas.drothler@web.de>
2026-02-07 15:16:42 +0100

    Update S3 client configuration to use 's3v4' signature version and enable payload signing with virtual addressing style

--- src/manager/s3_manager.py
+++ src/manager/s3_manager.py
@@ -56,8 +56,11 @@ class S3Manager:
             aws_access_key_id=self.access_key_id,
             aws_secret_access_key=self.secret_key,
             config=Config(
-                signature_version='s3',
-                s3={'addressing_style': 'path'}
+                signature_version='s3v4',
+                s3={
+                    'payload_signing_enabled': True,
+                    'addressing_style': 'virtual',
+                },
             )
         )
 
